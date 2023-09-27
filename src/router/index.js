31d0cd0dc0412dbd27d5f6980435137dcd469f6d
ptmhang <myhang1503@gmail.com>
2023-09-27 20:38:06 +0700

    contact, blog, rooms

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import BlogView from '../views/BlogView.vue'
+import RoomsView from "../views/RoomsView.vue";
+import RoomDetail from "../views/RoomDetail.vue";
+import ContactView from "../views/ContactView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: HomeView
+    path: "/",
+    name: "Home",
+    component: HomeView,
   },
   {
-    path: '/blog',
-    name: 'Blog',
+    path: "/blog",
+    name: "Blog",
     component: BlogView,
-  }
-]
+  },
+  {
+    path: "/rooms",
+    name: "Rooms",
+    component: RoomsView,
+  },
+  {
+    path: "/room-detail",
+    name: "RoomDetail",
+    component: RoomDetail,
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: ContactView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
